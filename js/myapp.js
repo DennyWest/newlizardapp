6fcf1d9d5ce36e786803955e133eeb6f9c1f87e7
dennywest <dennywest@Dennys-MacBook-Pro.local>
2014-12-18 13:30:09 -0800

    Changed stuff

---
+++ js/myapp.js
@@ -0,0 +1,25 @@
+function DOMLoaded() {
+	document.addEventListener("deviceready", phonegapLoaded, false);
+}
+
+function phonegapLoaded() {
+
+	}
+
+function takePicture() {
+
+	navigator.camera.getPicture(onSuccess, onFail, {quality: 70, destinationType: camera.DestinationType.FILE_URI});
+}
+
+function onSuccess(imageURI) {
+
+	var image = document.getElementById('image');
+	image.src = imageURI;
+
+	image.style.display = 'block';
+}
+
+function onFail(message){
+
+	alert('Failed because: ' + message);
+}
